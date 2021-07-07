81b4396b4f3253c28979e83b7bc0347eb1a74ff3
tanakaderoy <42681052+tanakaderoy@users.noreply.github.com>
2021-07-07 16:52:54 -0400

    Force rotate not working tablet

--- ForceRotate/ViewController.swift
+++ ForceRotate/ViewController.swift
@@ -13,7 +13,52 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .landscapeRight
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    @IBAction func lockLandscape(_ sender: Any) {
+        lockToLandscapeRight()
+    }
+    @IBAction func unlock(_ sender: Any) {
+        unlockOrientation()
+    }
+    
 
 
+    @objc
+    func lockToLandscapeLeft() {
+        setOrientation(.landscapeLeft, mask: .landscapeLeft)
+    }
+
+    @objc
+    func lockToLandscapeRight() {
+        setOrientation(.landscapeRight, mask: .landscapeRight)
+    }
+
+    @objc
+    func unlockOrientation() {
+        setOrientation(.unknown, mask: .all)
+    }
+
+    private func setOrientation(_ orientation: UIInterfaceOrientation, mask: UIInterfaceOrientationMask) {
+        DispatchQueue.main.async {
+            
+            UIDevice.current.setValue(UIInterfaceOrientation.unknown.rawValue, forKey: "orientation")
+            // This is a private API. `orientation` is a read-only value.
+            // See https://developer.apple.com/documentation/uikit/uidevice/1620053-orientation
+            // However this seems to be the industry accepted solution to forcing orientations
+            // so we are going to keep it. The alternative is to display something telling the user
+            // to actually rotate their device, and then disabling auto-rotate once they do so.
+            UIDevice.current.setValue(orientation.rawValue, forKey: "orientation")
+            UIViewController.attemptRotationToDeviceOrientation()
+        }
+    }
+    
 }
 
